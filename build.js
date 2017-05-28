b3bcc750f861238906c8d4e51beeb5c9af6fa1ad
Ahmed Fasih <wuzzyview@gmail.com>
2017-05-28 00:08:41 -0400

    Tags for posts, header, footer in

--- build.js
+++ build.js
@@ -2,7 +2,11 @@
 
 var fs = require('fs');
 var yaml = require('js-yaml');
+// var Promise = require("bluebird");
+var find = require('find');
 var spawnSync = require('child_process').spawnSync;
+
+var PREPATH = ''; // i.e., if site lives at foo.edu/~me, RELPATH='~me'.
 
 var css = '<style>' + fs.readFileSync('modest.css', 'utf8') + '</style>\n';
 
@@ -17,40 +21,137 @@ var defaultMeta = {
   draft : false,
 };
 
-function buildOneMarkdown(filepath) {
-  var outfile = filepath.replace(/md$/, 'html');
-  var contents = fs.readFileSync(filepath, 'utf8');
-  var meta = {};
-  if (contents.indexOf('---\n') === 0) {
-    meta = yaml.safeLoad(contents.substring(4, contents.indexOf('\n---\n', 4)));
-  }
-  var html = spawnSync('pandoc',
-                       [
-                         '--filter', 'filter.js', '--no-highlight', '-f',
-                         'markdown_github-hard_line_breaks+yaml_metadata_block',
-                         '-t', 'html5', filepath
-                       ],
-                       {input : undefined, encoding : 'utf8'})
-                 .stdout;
+var articles = new Map([]);
+
+function gatherIntel(filepath) {
+  var meta = fileToMeta(filepath);
+  articles.set(filepath, meta);
+}
+
+find.file(/\.md$/, '.', md => {
+  md = md.filter(s => s.indexOf('node_modules') !== 0);
+  md.forEach(gatherIntel);
+  var metas = Array.from(articles.values()).sort((a, b) => a.date - b.date);
+
+  var postMetas = metas.filter(meta => meta.filepath.indexOf('post/') === 0);
+  var nonpostMetas = metas.filter(meta => meta.filepath.indexOf('post/') !== 0);
+
+  var tagsToMetas = groupBys(postMetas, meta => meta.tags);
+
+  postMetas.forEach((meta, midx) => {
+    buildOneMarkdown(meta, postMetas[midx - 1], postMetas[midx + 1], postMetas,
+                     tagsToMetas);
+  });
+  nonpostMetas.forEach((meta, midx) => {
+    buildOneMarkdown(meta, null, null, metas, tagsToMetas);
+  });
+});
+
+// Build posts: have next/prev/tags + header
+// Build non-posts: have header
+// Build tags: have header
+
+function buildOneMarkdown(meta, prevMeta, nextMeta, metas, tagsToMetas) {
+  var filepath = meta.filepath;
+  var outfile = meta.outfile;
+  var html =
+      spawnSync(
+          'pandoc',
+          [
+            '--filter', 'filter.js', '--no-highlight', '-f',
+            'markdown_github-hard_line_breaks+yaml_metadata_block+markdown_in_html_blocks',
+            '-t', 'html5', filepath
+          ],
+          {input : undefined, encoding : 'utf8'})
+          .stdout;
 
   var head = '<head><meta charset="utf-8" />\n';
-  head += `<title>${meta.title || defaultMeta.title}</title>\n`
-  head += social(outfile, meta.title || defaultMeta.title,
-                 meta.description || defaultMeta.description,
-                 meta.socialBanner || meta.banner || defaultMeta.banner);
+  head += `<title>${meta.title}</title>\n`
+  head += social(outfile, meta.title, meta.description,
+                 meta.socialBanner || meta.banner);
   head += css;
   head += '\n</head>\n';
   head += meta.banner ? banner(meta.banner) : '';
-  head += headline(meta.title || defaultMeta.title);
+  head += headline(meta.title);
+  head += subline(meta.date, meta.tags);
+
   fs.writeFileSync(outfile, '');
   fs.appendFileSync(outfile, head);
   fs.appendFileSync(outfile, html);
+
+  var foothtml = prevNextToFoot(prevMeta, nextMeta);
+  if (foothtml) {
+    fs.appendFileSync(outfile, foothtml);
+  }
+}
+
+var path = require('path');
+function filepathToAbspath(filepath) {
+  return path.resolve('/', PREPATH, filepath).replace(/index.html$/, '');
+}
+function prevNextToFoot(prevMeta, nextMeta) {
+  if (prevMeta || nextMeta) {
+    // console.log(prevMeta, nextMeta);
+    var foot = `<p><small>`;
+    if (prevMeta) {
+      foot += `Previous:
+[${prevMeta.title}](${filepathToAbspath(prevMeta.outfile)})<br>`;
+    }
+    if (nextMeta) {
+      foot += `Next:
+[${nextMeta.title}](${filepathToAbspath(nextMeta.outfile)})`;
+    }
+    foot += '</small></p>'
+    var foothtml =
+        spawnSync('pandoc',
+                  [
+                    '-f',
+                    'markdown_github-hard_line_breaks+markdown_in_html_blocks',
+                    '-t', 'html5'
+                  ],
+                  {input : foot, encoding : 'utf8'})
+            .stdout;
+    return foothtml;
+  }
+  return '';
+}
+
+function fileToMeta(filepath) {
+  var contents = fs.readFileSync(filepath, 'utf8');
+  var outfile = filepath.replace(/md$/, 'html');
+  var meta = Object.assign({}, defaultMeta, {filepath, outfile});
+  if (contents.indexOf('---\n') === 0) {
+    meta = Object.assign(
+        {}, meta,
+        yaml.safeLoad(contents.substring(4, contents.indexOf('\n---\n', 4))));
+    if (meta.date) {
+      meta.date = new Date(meta.date);
+    }
+    if (meta.dateUpdated) {
+      meta.dateUpdated = new Date(meta.dateUpdated);
+    }
+  }
+  return meta;
+}
+
+function subline(date, tags) {
+  var text = '';
+  var tagtext = tags.map(s => `‘${s}’`).join('—');
+  if (date && (tags && tags.length)) {
+    text = `Updated on ${date.toUTCString()}, tagged with ${tagtext}.`;
+  } else if (date) {
+    text = `Updated on ${date.toUTCString()}.`;
+  } else if (tags && tags.length) {
+    text = `Tagged with ${tagtext}.`
+  }
+  return text ? `<p><em>${text}</em></p>` : '';
 }
 
 function banner(url) {
-  return `<figure class=""><img src="${url}"></figure>`;
+  // return `<figure class=""><img src="${url}"></figure>`;
   return `<figure class="full-width no-top"><img src="${url}"></figure>`;
 }
+
 function headline(title) { return `<h1>${title}</h1>`; }
 
 function social(url, title, description, banner) {
@@ -63,5 +164,16 @@ function social(url, title, description, banner) {
 <meta property="og:description" content="${description}" />\n`;
 }
 
-buildOneMarkdown('index.md')
-buildOneMarkdown('post/texshade/index.md')
+function groupBys(collection, keysfn) {
+  var db = {};
+  for (let val of collection) {
+    for (let key of keysfn(val)) {
+      if (db[key]) {
+        db[key].push(val);
+      } else {
+        db[key] = [ val ];
+      }
+    }
+  }
+  return db;
+}
